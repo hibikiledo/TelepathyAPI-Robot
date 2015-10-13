d592ab1c9a69a93acd5a5e6a60f11d13c18d335f
@hbk <nut.rainbowpuke@gmail.com>
2015-10-13 16:40:15 +0700

    Fix error passing 'action' instead of action variable

--- dispatcher.py
+++ dispatcher.py
@@ -5,7 +5,7 @@ class Dispatcher:
         self.handler_map = {}
 
     def handle(self, action, value):
-        self.handler_map['action'](value)
+        self.handler_map[action](value)
 
     def register(self, action, handler):
         self.handler_map[action] = handler
